0f1615c1793ec91c396f2f63870b02f3363c18bd
Joseph <josephebenge@gmail.com>
2018-08-05 14:09:21 -0400

    Implemented Mvvm friendly injection.

--- WpfNinjectProject/WpfInjection/Ninject/ServiceLocator.cs
+++ WpfNinjectProject/WpfInjection/Ninject/ServiceLocator.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfInjection.ViewModels;
 
 namespace WpfInjection.Ninject
 {
     public class ServiceLocator
     {
+        private readonly IKernel kernel;
+
+        public ServiceLocator()
+        {
+            kernel = new StandardKernel(new ServiceModule());
+        }
+
+        public AppViewModel AppViewModel
+        {
+            get { return kernel.Get<AppViewModel>(); }
+        }
     }
 }
--- WpfNinjectProject/WpfInjection/Ninject/ServiceModule.cs
+++ WpfNinjectProject/WpfInjection/Ninject/ServiceModule.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Ninject.Modules;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfInjection.Services;
 
 namespace WpfInjection.Ninject
 {
-    public class ServiceModule
+    public class ServiceModule : NinjectModule
     {
+        public override void Load()
+        {
+            Bind<IDialogService>().To<DialogService>();
+            Bind<IDataService>().To<DataService>();
+        }
     }
 }
